59a834d7d3f9d6df83095f07c7b110fba3c300c1
swapnil-kolavankar <swapnil.kolavankar15@gmail.com>
2024-07-03 17:35:23 +0530

    creating Food delivery web-app

---
+++ Food_Delivery_app/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Header.css"
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Choose a from diverse menu featuring a delectable array of dishes crafted with the finest ingredient and culinary expertise. Our mission is to satisfy your craving and elevate your dining experience.one delicious metal at a time.</p>
+        <button>View Menu</button>
+        </div>  
+    </div>
+  )
+}
+
+export default Header
